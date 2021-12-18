c5622b1406bdb75a6b6bc92aeca4c396c3a00071
chomp105 <54012005+chomp105@users.noreply.github.com>
2021-12-18 12:45:53 -0600

    Add files via upload

---
+++ ttt.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void printboard(int board[3][3]) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("%d", board[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void move(int board[3][3], int player) {
+	int x, y;
+	scanf("%d", &x);
+	scanf("%d", &y);
+	board[y][x] = player;
+}
+
+int checkwin(int board[3][3]) {
+	for (int i = 0; i < 3; i++) {
+		if ((board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] != 0) ||
+		    (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[2][i] != 0)) {
+			return 1;
+		}
+	}
+	if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != 0) ||
+	    (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != 0)) {
+		return 1;
+	}
+	return 0;
+}
+
+int main(void) {
+	int board[3][3] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+	//int player = 1;
+	for (int i = 0; i < 9; i++) {
+		printboard(board);
+		move(board, i % 2 + 1);
+		if (checkwin(board)) {
+			break;
+		}
+		//player = player == 1 ? 2 : 1;
+	}
+	return 0;
+}
